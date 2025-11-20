4241e895e964845a8dea55ba81667119d8eda2f7
kamoline67 <kamoline87@gmail.com>
2025-11-20 19:08:00 -0300

    docker db

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,20 @@
 const { Sequelize } = require('sequelize');
 
-const conexao = new Sequelize('dbpetday', 'root', 'escola', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
+const conexao = new Sequelize(
+  process.env.DB_NAME || 'petday',
+  process.env.DB_USER || 'root',
+  process.env.DB_PASS || '1234',
+  {
+    host: process.env.DB_HOST || 'db',
+    port: process.env.DB_PORT || 3306,
+    dialect: 'mysql',
+    logging: console.log,
+    retry: {
+      max: 10,
+      timeout: 30000
+    }
+  }
+);
 
 conexao.authenticate()
   .then(() => {
